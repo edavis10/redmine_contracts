a80b308418c276c60389c48feb5ce0a1bf26a972
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-17 10:10:28 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_contracts do
+  name 'Redmine Contracts plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
