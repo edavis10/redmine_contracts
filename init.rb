52cf09973a3759acc2bc56aa3d3c66d9b168df51
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-17 11:18:08 -0700

    Configure init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,10 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_contracts do
   name 'Redmine Contracts plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Eric Davis'
+  description 'A system to manage the execution of a client contract by separating it into deliverables and milestones.'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-contracts'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+  requires_redmine :version_or_higher => '0.9.0'
 end
