1138616e7717ab59cb2119fc890dd311e686bbeb
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-17 12:13:14 -0700

    [#4177] Added Contract associations.

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,3 +1,8 @@
 class Contract < ActiveRecord::Base
   unloadable
+
+  # Associations
+  belongs_to :project
+  belongs_to :account_executive, :class_name => 'User', :foreign_key => 'account_executive_id'
+  
 end
--- test/unit/contract_test.rb
+++ test/unit/contract_test.rb
@@ -1,4 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ContractTest < ActiveSupport::TestCase
+  should_belong_to :account_executive
+  should_belong_to :project
 end
