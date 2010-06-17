abfc52f46b84c10bbfe3ba3dbcf6bb2fbd964b30
Eric Davis <edavis@littlestreamsoftware.com>
2010-06-17 14:00:13 -0700

    Hook up webrat

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper
 
 # Ensure that we are using the temporary fixture path
 Engines::Testing.set_fixture_path
+
+require "webrat"
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
