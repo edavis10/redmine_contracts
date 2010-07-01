19958fe16e42aa500f6d6048e8c453e66b768836
Eric Davis <edavis@littlestreamsoftware.com>
2010-07-01 14:48:35 -0700

    [#4181] Require a Deliverable's title, type, and manager.

--- app/models/deliverable.rb
+++ app/models/deliverable.rb
@@ -6,10 +6,24 @@ class Deliverable < ActiveRecord::Base
   belongs_to :manager, :class_name => 'User', :foreign_key => 'manager_id'
 
   # Validations
-
+  validates_presence_of :title
+  validates_presence_of :type
+  validates_presence_of :manager
+  
   # Accessors
 
   def short_type
     ''
   end
+
+  if Rails.env.test?
+    generator_for :title, :method => :next_title
+
+    def self.next_title
+      @last_title ||= 'Deliverable 0000'
+      @last_title.succ!
+    end
+
+  end
+  
 end
--- test/unit/deliverable_test.rb
+++ test/unit/deliverable_test.rb
@@ -3,4 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class DeliverableTest < ActiveSupport::TestCase
   should_belong_to :contract
   should_belong_to :manager
+
+  should_validate_presence_of :title
+  should_validate_presence_of :type
+  should_validate_presence_of :manager
 end
