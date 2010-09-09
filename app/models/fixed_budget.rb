07da5d98d8e0c6bce58930dc79affe17ab944aaf
Eric Davis <edavis@littlestreamsoftware.com>
2010-09-09 16:12:48 -0700

    [#4477] Added the FixedBudget markup calculations.

--- app/models/fixed_budget.rb
+++ app/models/fixed_budget.rb
@@ -7,4 +7,27 @@ class FixedBudget < ActiveRecord::Base
   # Validations
 
   # Accessors
+  def markup_value
+    return 0 if budget.blank? || markup.blank?
+
+    case
+    when percent_markup?
+      percent = markup.gsub('%','').to_f
+      return budget.to_f * (percent / 100)
+    when straight_markup?
+      markup.gsub('$','').gsub(',','').to_f
+    else
+      0 # Invalid markup
+    end
+    
+  end
+
+  def percent_markup?
+    markup && markup.match(/%/)
+  end
+
+  def straight_markup?
+    markup && markup.match(/\$/)
+  end
+  
 end
