9cacef1299277f348f98165bdb4ba6093a2d8d6c
Eric Davis <edavis@littlestreamsoftware.com>
2012-01-19 11:35:51 -0800

    Only load jquery.js if it isn't being loaded by the app

--- lib/redmine_contracts/hooks/view_layouts_base_html_head_hook.rb
+++ lib/redmine_contracts/hooks/view_layouts_base_html_head_hook.rb
@@ -6,15 +6,25 @@ module RedmineContracts
                                     context[:controller].is_a?(ContractsController) ||
                                     context[:controller].is_a?(DeliverablesController)
                                     )
-          return stylesheet_link_tag("redmine_contracts", :plugin => "redmine_contracts", :media => "screen") +
-            stylesheet_link_tag('smoothness/jquery-ui-1.8.15.custom.css', :plugin => "redmine_contracts") +
+          tags = [stylesheet_link_tag("redmine_contracts", :plugin => "redmine_contracts", :media => "screen")]
+          tags << stylesheet_link_tag('smoothness/jquery-ui-1.8.15.custom.css', :plugin => "redmine_contracts")
 
-            javascript_include_tag('jquery-1.4.4.min.js', :plugin => 'redmine_contracts') +
-            javascript_include_tag('jquery.tmpl.min.js', :plugin => 'redmine_contracts') +
-            javascript_include_tag('jquery-ui-1.8.15.custom.min.js', :plugin => "redmine_contracts") +
-            javascript_tag('jQuery.noConflict();') +
-            javascript_include_tag('contracts.js', :plugin => 'redmine_contracts')
+          jquery_included = begin
+                              ChiliProject::Compatibility && ChiliProject::Compatibility.using_jquery?
+                          rescue NameError
+                            # No compatibilty test
+                            false
+                          end
+        unless jquery_included
+          tags << javascript_include_tag('jquery-1.4.4.min.js', :plugin => 'redmine_contracts')
+          tags << javascript_tag('jQuery.noConflict();')
+        end
+          
+          tags << javascript_include_tag('jquery.tmpl.min.js', :plugin => 'redmine_contracts')
+          tags << javascript_include_tag('jquery-ui-1.8.15.custom.min.js', :plugin => "redmine_contracts")
+          tags << javascript_include_tag('contracts.js', :plugin => 'redmine_contracts')
 
+          return tags.join(' ')
         else
           return ''
         end
